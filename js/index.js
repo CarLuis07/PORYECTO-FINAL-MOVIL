8c225d93b16821b24a59a11a854c89d19a03444c
CARDONA <91144533+CarLuis07@users.noreply.github.com>
2023-06-12 15:50:34 -0600

    PROYECTO FINAL MOVIL 2.0

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var url = "https://apipw1.myferby.com/ws.php?tk=t308218823&op="
+var url = "https://apipw1.myferby.com/ws.php?tk=t32311277&op="
 
 CategoriasActivasDelSistema();
 
@@ -14,22 +14,22 @@ function CategoriasActivasDelSistema() {
                 var div1 = document.createElement("div");
                 var img = document.createElement("img");
                 var div2 = document.createElement("div");
-                var h5 = document.createElement("h5");
+                var h6 = document.createElement("h6");
 
                 categorias.appendChild(div1);
                 div1.append(img, div2);
-                div2.appendChild(h5);
+                div2.appendChild(h6);
 
                 div1.classList = "card ms-2 mt-3";
                 div1.setAttribute("style", "width: 8rem");
                 img.classList = "card-img-top mt-2";
                 img.setAttribute("height", "110px");
                 div2.classList = "card-body";
-                h5.classList = "card-title";
+                h6.classList = "card-title";
                 div2.setAttribute("style", "text-align: center");
 
                 img.src = c.imagen;
-                h5.innerHTML = c.nombre;
+                h6.innerHTML = c.nombre;
 
             })
         })
